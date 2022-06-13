1d52d4aa9facd38f54f89d6dc1b9c9de1b1351d6
Srija Gurram <srijagurram16@gmail.com>
2022-06-13 15:41:33 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
 //import Navbar from './components/Navbar';
-import AddAchievement from "./components/AddAchievement";
-import Home from "./components/Home";
-import { PageLayout } from "./components/PageLayout";
+import AddAchievement from "./Components/AddAchievement";
+import Home from "./Components/Home";
+import { PageLayout } from "./Components/PageLayout";
  import {
    Route,
    BrowserRouter as Router,
    Routes,
    Switch
  } from "react-router-dom";
+import Achievements from "./Components/Achievements";
+import Approvals from "./Components/Approvals";
+import LoginView from "./Components/Views/LoginView";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
     <PageLayout>
       <Routes>
-      {/* <Route exact path ="/" render={()=>(
-        <Home/>
-      )} /> */}
+      <Route exact path ="/" element={<LoginView/>}/>
         <Route exact path="/Home" element={<Home/>} />
         <Route exact path="/AddAchievement" element={<AddAchievement/>} />
+        <Route exact path="/Achievements" element={<Achievements/>} />
+        <Route exact path="/Profile" element={<Profile/>} />
+        <Route exact path="/Approvals" element={<Approvals/>}/>
       </Routes>
     </PageLayout>
   );
--- src/authConfig.js
+++ src/authConfig.js
@@ -1,22 +1,60 @@
 export const msalConfig = {
-    auth: {
-      clientId: "2b0b36e1-614a-4b05-af9f-65c23916f4ac",
-      authority: "https://login.microsoftonline.com/b1cce2ac-5c07-40fe-91cc-09102bfa2522", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
-      redirectUri: "http://localhost:3000",
-      navigateToLoginRequestUrl: true
-    },
-    cache: {
-      cacheLocation: "sessionStorage", // This configures where your cache will be stored
-      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-    }
-  };
+     auth: {
+       clientId: "2b0b36e1-614a-4b05-af9f-65c23916f4ac",
+       authority: "https://login.microsoftonline.com/b1cce2ac-5c07-40fe-91cc-09102bfa2522", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+       redirectUri: "http://localhost:3000/Home",
+       navigateToLoginRequestUrl: false,
+     },
+     cache: {
+       cacheLocation: "sessionStorage", // This configures where your cache will be stored
+       storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+     }
+   };
   
-  // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
-  export const loginRequest = {
-   scopes: ["User.Read"]
-  };
-  
-  // Add the endpoints here for Microsoft Graph API services you'd like to use.
-  export const graphConfig = {
-      graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
-  };
+   // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+   export const loginRequest = {
+    scopes: ["User.Read"]
+   };
+ 
+   // Add the endpoints here for Microsoft Graph API services you'd like to use.
+   export const graphConfig = {
+       graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+   };
+
+// import { useIsAuthenticated } from "@azure/msal-react";
+// import { Navigate } from "react-router-dom";
+
+// export const msalConfig = {
+//   auth: {
+//     clientId: "2b0b36e1-614a-4b05-af9f-65c23916f4ac",
+//     authority:
+//       "https://login.microsoftonline.com/b1cce2ac-5c07-40fe-91cc-09102bfa2522", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+//     redirectUri: "http://localhost:3000/Home",
+//     navigateToLoginRequestUrl: true,
+//   },
+//   cache: {
+//     cacheLocation: "localStorage", // This configures where your cache will be stored
+//     storeAuthStateInCookie: true, // Set this to "true" if you are having issues on IE11 or Edge
+//   },
+// };
+
+// // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+// export const loginRequest = {
+//   scopes: ["User.Read"],
+// };
+
+// // Add the endpoints here for Microsoft Graph API services you'd like to use.
+// export const graphConfig = {
+//   graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
+// };
+
+// // export const ProtectedRoute = ({ children }) => {
+// //   const isAuthenticated = useIsAuthenticated();
+
+// //   return isAuthenticated ? children : <Navigate to={"/"} />;
+// // };
+// //  export const RedirectLoggedUser = ({ children }) => {
+// //    const isAuthenticated = useIsAuthenticated();
+
+// //    return !isAuthenticated ? children : <Navigate to={"/Home"} />;
+// //  };
