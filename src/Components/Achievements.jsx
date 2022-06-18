43eaf2445e70a58e23c386433704c34c19e1ca14
Srija Gurram <srijagurram16@gmail.com>
2022-06-18 16:44:53 +0530

    Adding achievements

--- src/Components/Achievements.jsx
+++ src/Components/Achievements.jsx
@@ -8,6 +8,10 @@ import SideBar from "./SideBar";
 //import UserProfile from "./UserProfile";
 //<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 //import swal from 'sweetalert'
+import pending from '../assests/pending_final.png'
+import approved from '../assests/approve_final.png'
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+
 const admin = {
     width: '100%',
     height: '100%',
@@ -25,184 +29,80 @@ class Achievements extends React.Component {
     constructor(props) {
         super(props);
          this.state={
-           achievements:[{
-            "id": 3,
-            "category": {
-              "categoryId": 1,
-              "name": "Udemy",
-              "points": 200
-            },
-            "user": {
-              "usersId": 1,
-              "name": "Akshay Kumar",
-              "email": "akshay.kumar.1@hexagon.com",
-              "managerId": 2,
-              "isManager": "False",
-              "redeemed": 0,
-              "claimed": 2200,
-              "total": 2200
-            },
-            "status": "Approved",
-            "proof": "https://udemy-certificate.s3.amazonaws.com/image/UC-9f515558-1e29-409b-829a-be57215c5171.jpg?v=1633364328000",
-            "completedOn": "2021-10-04T00:00:00",
-            "description": "Agile fundamental course"
-          },
-          {
-            "id": 4,
-            "category": {
-              "categoryId": 2,
-              "name": "External Certification",
-              "points": 2000
-            },
-            "user": {
-              "usersId": 1,
-              "name": "Akshay Kumar",
-              "email": "akshay.kumar.1@hexagon.com",
-              "managerId": 2,
-              "isManager": "False",
-              "redeemed": 0,
-              "claimed": 2200,
-              "total": 2200
-            },
-            "status": "Approved",
-            "proof": "someurl",
-            "completedOn": "2021-12-07T00:00:00",
-            "description": "PoC on porting heavy computation to cloud"
-          },
-          {
-            "id": 1002,
-            "category": {
-              "categoryId": 5,
-              "name": "Advent of Code",
-              "points": 25000
-            },
-            "user": {
-              "usersId": 1,
-              "name": "Akshay Kumar",
-              "email": "akshay.kumar.1@hexagon.com",
-              "managerId": 2,
-              "isManager": "False",
-              "redeemed": 0,
-              "claimed": 2200,
-              "total": 2200
-            },
-            "status": "Pending",
-            "proof": "https://udemy-certificate.s3.amazonaws.com/image/UC-9f515558-1e29-409b-829a-be57215c5171.jpg?v=1633364328000",
-            "completedOn": "2022-06-07T00:00:00",
-            "description": "Completed"
-          },
-          {
-            "id": 2002,
-            "category": {
-              "categoryId": 1,
-              "name": "Udemy",
-              "points": 200
-            },
-            "user": {
-              "usersId": 1,
-              "name": "Akshay Kumar",
-              "email": "akshay.kumar.1@hexagon.com",
-              "managerId": 2,
-              "isManager": "False",
-              "redeemed": 0,
-              "claimed": 2200,
-              "total": 2200
-            },
-            "status": "Pending",
-            "proof": "https://udemy-certificate.s3.amazonaws.com/image/UC-9f515558-1e29-409b-829a-be57215c5171.jpg?v=1633364328000",
-            "completedOn": "2022-06-09T00:00:00",
-            "description": "Completed"
-          }],
+          achievements:[]
            }
     }
       
-    renderResultRows(){
+   
+    achievements(){
+      let userID = 1;
+      const url = "https://localhost:44355/Achievement/"+userID;
+
+      //  var body = {
+      //   userID:1,
+      //  }
+       let headers = new Headers();
+       headers.append('Content-Type','application/json');
+      //  headers.append('Accept','application/json');
+      //  headers.append('Access-Control-Allow-origin',url);
+      //  headers.append('Access-Control-Allow-Credentials','true');
+      //  headers.append('POST','GET');
       
-        let s=this.state.achievements
-        let n
-        // sessionStorage.setItem("status",s.status);
-        // sessionStorage.setItem("category",s.category.name);
-        // sessionStorage.setItem("description",s.description);
-        // sessionStorage.setItem("points",s.category.points);
-       
-        return s.map((achievement) => {
-          <tr id={achievement.id}> 
-                   <td >{achievement.category.name}</td>
-                   <td >{achievement.description}</td>
-                   <td >{achievement.category.points}</td>
-                   <td>{achievement.status}</td>
-          </tr>
-       });  
-    
-      
+        fetch(url,{
+           headers:headers,
+           method: 'GET',
+           //body:JSON.stringify()
+           })
+           .then(response=>{return response.json()})
+           .then(res=>{this.setState({achievements:res})
+
+           console.log(this.state.achievements)
+           //sessionStorage.setItem("category",this.state.achievements.category)
+           
+       })
+          
+          
+     }
+     renderStatus(status,value)
+     {
+      return(
+        <div className={styles.status}>
+        <MDBCard style={{ width: '50px' }} >
+            <MDBCardImage src={status} alt='...' position='top' />
+          </MDBCard>
+          <MDBCardBody>
+          <MDBCardText>
+            {value}
+          </MDBCardText>
+        </MDBCardBody>
+        </div>
+      );
+     }
+    componentDidMount(){
+       this.achievements()
     }
-
-    // items(){
-    //   const url = "https://localhost:44355/Achievement";
-
-    //   var body = {
-    //     owner:sessionStorage.getItem("userID"),
-    //   }
-    //   let headers = new Headers();
-    //   headers.append('Content-Type','application/json');
-    //   headers.append('Accept','application/json');
-
-    //   headers.append('Access-Control-Allow-origin',url);
-    //   headers.append('Access-Control-Allow-Credentials','true');
-
-    //   headers.append('POST','GET');
-      
-    //    fetch(url,{
-    //       headers:headers,
-    //       method: 'POST',
-    //       body:JSON.stringify(body)
-    //       })
-    //       .then(response=>{console.log(response.json)              
-    //           return response.json()
-    //       }).then(res=>{this.setState({achievements:res})
-    //   })
-          
-          
-    // }
-        // componentDidMount(){
-        //   this.achievements()
-        // }
     
         render() {
-        
-          var data = this.state.achievements;
             return (
-            <div>
-                {/* <SideBar/> */}
-                  <div className="admin">
-                      
-                      <div className={styles.achievement}>
-                        <table className="w3-table-all">
-                        <thead>
-                          <tr>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                          </tr>
-                          </thead>
-                        <tbody> {this.renderResultRows()}</tbody>
-                        {/* <tbody>
-                        {this.state.achievements.map((achievement) => 
-                              <tr id={achievement.id} class="tr"> 
-                                      <td >{achievement.category}</td>
-                                      <td >{achievement.description}</td>
-                                      <td >{achievement.points}</td>
-                                      <td>{achievement.user.status}</td>
-                              </tr>
-                        )}
-                        </tbody> */}
-                        </table>
-                        </div>
-                    </div>
-                    <br></br>
-                    <br></br>
-            </div>
+            <center>
+            {this.state.achievements.map((achievement, myKey) =>
+            <MDBCard className={styles.achievement}>
+              <MDBCardBody>
+                <MDBCardTitle>{achievement.category.name}</MDBCardTitle>
+                <MDBCardText>
+                {achievement.description}
+                </MDBCardText>
+                {(achievement.status==='Pending'?this.renderStatus(pending,achievement.status):this.renderStatus(approved,achievement.status))}
+                <br/>
+                <br/>
+                <br/>
+                <label className={styles.points_label}>{achievement.category.points} pts</label>
+              </MDBCardBody>
+            </MDBCard>
+              )}
+
+            </center>
+          
     );
         }
 }
